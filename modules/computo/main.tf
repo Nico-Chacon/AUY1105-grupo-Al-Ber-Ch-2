5c1ef4ee97d969d70ddc3df1774f4c6c67523137
NICKO_LARZ <nicochacon531@gmail.com>
2026-06-05 09:31:09 +0000

    fix pt2

--- modules/computo/main.tf
+++ modules/computo/main.tf
@@ -1,20 +1,15 @@
 resource "aws_instance" "duocapp" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id     = var.subnet_id
+  ebs_optimized = true
+  vpc_security_group_ids = [var.security_group_id]
 
-  monitoring = true   # Activa detailed monitoring
-
-  metadata_options {
-    http_tokens = "required"   # Forzar IMDSv2
+  tags = {
+    Name = var.instance_name
   }
-
-  associate_public_ip_address = false  # Quita IP pública si no es necesaria
-}
-
-root_block_device {
+  root_block_device {
   encrypted = true
+  }
 }
 
-ebs_optimized = true
