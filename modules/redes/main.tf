669ab0be1a02f26ab852e95bae53aeec90a97acb
NICKO_LARZ <nicochacon531@gmail.com>
2026-06-05 09:35:24 +0000

    fix pt2

--- modules/redes/main.tf
+++ modules/redes/main.tf
@@ -1,15 +1,24 @@
 resource "aws_vpc" "AUY1105-duocapp-vpc" {
   cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 resource "aws_subnet" "AUY1105-duocapp-subnet" {
-  vpc_id            = aws_vpc.AUY1105-duocapp-vpc.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = var.availability_zone
+  vpc_id                  = aws_vpc.AUY1105-duocapp-vpc.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_name
+  }
 }
 
 resource "aws_security_group" "example" {
-  name        = "example-sg"
+  name        = var.sg_name
   description = "SG para instancias EC2"
   vpc_id      = aws_vpc.AUY1105-duocapp-vpc.id
 
@@ -17,45 +26,19 @@ resource "aws_security_group" "example" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-    description = "Acceso SSH desde mi IP"
+    cidr_blocks = [var.allowed_ssh_cidr]
+    description = "Acceso SSH desde rango permitido"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Salida a internet"
+    description = "Salida HTTPS"
+  }
+
+  tags = {
+    Name = var.sg_name
   }
 }
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.AUY1105-duocapp-vpc.id
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.AUY1105-duocapp-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.AUY1105-duocapp-subnet.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_cloudwatch_log_group" "vpc_logs" {
-  name              = "vpc-flow-logs"
-  retention_in_days = 7
-}
-
-resource "aws_flow_log" "vpc" {
-  vpc_id          = aws_vpc.AUY1105-duocapp-vpc.id
-  log_destination = aws_cloudwatch_log_group.vpc_logs.arn
-  traffic_type    = "ALL"
-}
-
